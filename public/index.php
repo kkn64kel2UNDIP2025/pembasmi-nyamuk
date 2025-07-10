ff092e6cd97f122fd87da2c9b86760c8532c55a4
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2025-07-10 20:06:37 +0700

    Solve little problem at tester index.php

--- public/index.php
+++ public/index.php
@@ -58,7 +58,6 @@ require $paths->systemDirectory . '/Boot.php';
 
 exit(Boot::bootWeb($paths));
 
-<?php
 file_put_contents('/tmp/ci4-debug.txt', 'Masuk ke index.php'.PHP_EOL, FILE_APPEND);
 require_once '../vendor/autoload.php';
 $app = require_once '../app/Config/Boot/production.php';
